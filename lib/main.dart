700915ecd89f807d87778550110d5d7d456764a4
margarote <joaovitortur1@gmail.com>
2025-12-14 11:39:58 -0300

    feat: initial flutter project setup

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import 'src/core/theme/app_theme.dart';
+import 'src/features/pokemon/presentation/pages/home_page.dart';
+import 'src/injection_container.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  injector.init();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pokedex',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.light,
+      home: const HomePage(),
+    );
+  }
+}
